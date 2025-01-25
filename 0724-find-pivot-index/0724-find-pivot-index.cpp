a506330270b77d809dea7c2f3e6fa997fd0bcdd5
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-01-25 14:42:41 +0530

    Time: 802 ms (5.09%), Space: 35.9 MB (29.46%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+int bruteForce(vector<int>& nums){
+
+    for(int i = 0; i<nums.size(); ++i){
+        int leftsum = 0;
+        int rightsum = 0;
+
+    
+    for(int j = 0; j < i; ++j ){
+        leftsum += nums[j];
+    }
+    for(int j = i+1; j<nums.size(); ++j){
+        rightsum += nums[j];
+    }
+
+    if(leftsum == rightsum ) return i;
+    }
+    return -1;
+    
+}
+
+    int pivotIndex(vector<int>& nums) {
+      return bruteForce(nums);  
+    }
+};
