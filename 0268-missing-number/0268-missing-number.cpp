f65fc20c17afa45909de69c580827a9e44bfc42a
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-01-26 22:31:24 +0530

    Time: 0 ms (100%), Space: 21.6 MB (92.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
 
-int sortingMethod(vector<int>& nums){
-    sort(nums.begin(), nums.end());
-    for(int i = 0; i< nums.size(); ++i){
-        if(nums[i] == i) continue;
-        else 
-        return i;
+// int sortingMethod(vector<int>& nums){
+//     sort(nums.begin(), nums.end());
+//     for(int i = 0; i< nums.size(); ++i){
+//         if(nums[i] == i) continue;
+//         else 
+//         return i;
+//     }
+//     return nums.size();
+// }
+
+int bitManipulation(vector<int>& nums){
+    int ans = 0;
+    for(int i = 0; i < nums.size(); ++i){
+        ans ^= nums[i];
     }
-    return nums.size();
-} 
+    int n = nums.size();
+    for(int i = 0; i <= n; ++i){
+        ans ^= i;
+    }
+    return ans;
+}
+  
+  
     int missingNumber(vector<int>& nums) {
-        return sortingMethod(nums);
+        //return sortingMethod(nums);
         
+        return bitManipulation(nums);
     }
 };
