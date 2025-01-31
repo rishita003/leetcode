9ccb94a76952ea3de0dff7f686cc063d01ba05e4
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-01-31 12:27:32 +0530

    Time: 0 ms (100%), Space: 11.5 MB (91.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int l=0, m=0, h = nums.size()-1;
+        while(m<=h){
+            if(nums[m]==0){
+            swap(nums[l], nums[m]);
+            l++, m++;}
+
+            else if(nums[m]==1){
+            m++;}
+
+            else{
+            swap(nums[m], nums[h]);
+            h--;}
+        }
         
     }
 };
