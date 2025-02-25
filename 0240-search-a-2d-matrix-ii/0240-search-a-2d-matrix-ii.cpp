26fe649cd2c2cd43bf67c6900cf90432850f623f
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-02-25 15:51:50 +0530

    Time: 50 ms (73.18%), Space: 18.7 MB (67.38%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,15 +4,30 @@ public:
         int m = matrix.size();    
         int n = matrix[0].size(); 
 
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == target) {
-                    return true; 
-                }
+        int row = 0,col = n-1;
+         while(row<m && col >=0){
+            if(matrix[row][col] == target){
+                return true;
             }
-        }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
 
-        return false;
+         }
+
+         return false;
+
+        
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (matrix[i][j] == target) {
+        //             return true; 
+        //         }
+        //     }
+        // }
+
     }
 };
