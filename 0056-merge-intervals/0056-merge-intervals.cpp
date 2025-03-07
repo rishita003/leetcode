a1a81c55e40e6293b5dcc119e781b24deb5d5987
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-03-07 12:24:58 +0530

    Time: 7 ms (44.23%), Space: 23.8 MB (59.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,23 +5,14 @@ public:
 
         vector<vector<int>> ans;
 
+
         for(int i = 0; i<intervals.size(); i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!ans.empty() && end<= ans.back()[1]){
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-
-            for(int j = i+1; j<intervals.size(); j++){
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            ans.push_back({start, end});
         }
         return ans;
     }
