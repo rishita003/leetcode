553ca3073b5fe158b772114c0f9ace9cba67a3a6
Rishita Sharma <111162990+rishita003@users.noreply.github.com>
2025-04-28 10:40:29 +0530

    Time: 1 ms (42.24%), Space: 96 MB (19.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        vector<int> merged;
+        int i = 0, j = 0;
         
-        int m = nums1.size();
-        int n = nums2.size();
-        int low = 0, high = m;
-        
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = (m + n + 1) / 2 - partitionX;
-            
-            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
-            
-            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
-            
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                if ((m + n) % 2 == 0) {
-                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
-                } else {
-                    return double(max(maxLeftX, maxLeftY));
-                }
-            } else if (maxLeftX > minRightY) {
-                high = partitionX - 1; 
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                merged.push_back(nums1[i]);
+                i++;
             } else {
-                low = partitionX + 1;  
+                merged.push_back(nums2[j]);
+                j++;
             }
         }
         
-        throw invalid_argument("Input arrays are not sorted properly");
+        while (i < nums1.size()) {
+            merged.push_back(nums1[i]);
+            i++;
+        }
+        
+        while (j < nums2.size()) {
+            merged.push_back(nums2[j]);
+            j++;
+        }
+        
+        int n = merged.size();
+        
+        if (n % 2 == 0) {
+            return (merged[n/2 - 1] + merged[n/2]) / 2.0;
+        } else {
+            return merged[n/2];
+        }
     }
 };
